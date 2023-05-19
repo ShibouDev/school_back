646ccc7e8e07fc378bc8f8918540dffacc29f882
ShibouDev <sadandcryalone@gmail.com>
2023-05-20 00:01:37 +0500

    wip: connect database, users resource

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {cors: false});
+  app.enableCors({credentials: true, origin: true})
+
+  const config = new DocumentBuilder()
+  .setTitle('SchoolAPI')
+  .setDescription('The school API description')
+  .setVersion('1.0')
+  .addTag('school')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger', app, document)
+
+  await app.listen(5000);
 }
 bootstrap();
